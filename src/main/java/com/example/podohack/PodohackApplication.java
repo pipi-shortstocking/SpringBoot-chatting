7a95cf1b02ed8be6424298881cad288b03fbc8b1
pipi-shortstocking <jannie435@naver.com>
2024-12-24 17:50:30 +0900

    init

---
+++ src/main/java/com/example/podohack/PodohackApplication.java
@@ -0,0 +1,13 @@
+package com.example.podohack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodohackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodohackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/podohack/PodohackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.podohack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodohackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
