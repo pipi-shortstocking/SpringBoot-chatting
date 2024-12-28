b5f5c82a0a4622fdf3b0e4db765caf8dbc7e3731
pipi-shortstocking <jannie435@naver.com>
2024-12-28 17:56:15 +0900

    유저 인증 인터셉터(문자열 확인 방식)

--- src/main/java/com/example/podohack/WebSocketConfig.java
+++ src/main/java/com/example/podohack/WebSocketConfig.java
@@ -15,6 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     // WebSocket을 Spring 애플리케이션에 등록하고, 특정 경로에서 WebSocket 요청을 처리하도록 설정하는 클래스
 
     private final WebSocketChatHandler webSocketHandler;
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     //  WebSocket 연결을 위한 설정
     // WebSocket 연결 End-Point: ws://localhost:8080/chats에 연결 시, 동작할 핸들러는 WebSocketChatHandler
@@ -22,6 +23,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/chats")
+                .addInterceptors(webSocketAuthInterceptor)
                 .setAllowedOrigins("*"); // 모든 도메인 허용 (CORS)
     }
 }
